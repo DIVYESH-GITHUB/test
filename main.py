656b73f3c2076efb8e31c2d9f29120b0ca0db762
DIVYESH-GITHUB <divyeshpindaria09@gmail.com>
2024-02-22 09:22:00 +0530

    test 2

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 app = FastAPI()
 
@@ -9,6 +15,6 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/items")
+async def create_item(item: Item):
+    return item
